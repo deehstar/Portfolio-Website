8a8229abcb01e9ad5ca1a2f8239862f251632105
deehstar <moand22@student.sdu.dk>
2024-04-08 13:12:40 +0200

    Project and Tag model created

---
+++ src/app/_models/Tag.ts
@@ -0,0 +1,17 @@
+export class Tag {
+
+    static readonly ANGULAR = new Tag('Angular', 'Red');
+    static readonly JAVA = new Tag('Java', 'royalblue');
+    static readonly TYPESCRIPT = new Tag('TypeScript', 'darkred');
+    static readonly ASPNET = new Tag('ASP.NET', 'purple');
+    static readonly JAVASCRIPT = new Tag('JavaScript', 'orange');
+    static readonly JavaSpring = new Tag('Java Spring', 'springgreen');
+    static readonly NODEJS = new Tag('Node.JS', 'brown');
+
+    private constructor(private readonly key: string, public readonly color: string) {
+
+    }
+    toString() {
+        return this.key;
+    }
+}
